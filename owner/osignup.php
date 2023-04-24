3586579bd038d954c08a2808ac07a8d80542fbb6
divineisking <josefdivine162@gmail.com>
2023-04-24 13:15:58 +0100

    fixed owner sign up

--- owner/osignup.php
+++ owner/osignup.php
@@ -12,23 +12,19 @@ if (isset($_COOKIE['user_id'])) {
 
 
 if(isset($_POST['submit'])){
-    
+
     $id = create_unique_id();
     $firstname = $_POST['firstname'];
-    $firtname = filter_var($firstname, FILTER_SANITIZE_STRING);
     $lastname = $_POST['lastname'];
-    $lastname = filter_var($lastname, FILTER_SANITIZE_STRING);
     $email = $_POST['email'];
-    $email = filter_var($email, FILTER_SANITIZE_STRING);
     $phone = $_POST['phone'];
-    $phone = filter_var($phone, FILTER_SANITIZE_STRING);
-    $password = sha1($_POST['password']);
-    $password = filter_var($password, FILTER_SANITIZE_STRING);
-    $c_password = sha1($_POST['c_password']);
-    $c_password = filter_var($c_password, FILTER_SANITIZE_STRING);
+    $password = hash('sha256', $_POST['password']);
+    $c_password = hash('sha256', $_POST['c_password']);
+    $_auth = hash('sha256', $email.$c_password);
 
 
-    $select_email = $conn->prepare("SELECT * FROM `owner` WHERE  email = ?");
+
+    $select_email = $conn->prepare("SELECT * FROM owner2 WHERE  email = ?");
     $select_email-> execute([$email]);
 
     if ($select_email->rowCount() > 0) {
@@ -37,25 +33,25 @@ if(isset($_POST['submit'])){
         if($password != $c_password){
             $warning_msg[] = 'incorrect password';
         }else{
-            $insert_user = $conn->prepare("INSERT INTO `owner`(id, firstname,
-             lastname, email, phone, password) VALUES(?,?,?,?,?,?)");
-            $insert_user->execute([$id, $firstname, $lastname, $email, $phone, $c_password]);
+            $insert_user = $conn->prepare("INSERT INTO owner2 (id, firstname,
+             lastname, email, phone, c_password, _auth) VALUES(?,?,?,?,?,?,?)");
+            $insert_user->execute([$id, $firstname, $lastname, $email, $phone, $c_password, $_auth]);
 
-            if($insert_user){
-                $verify_user = $conn->prepare("SELECT * FROM `owner` WHERE email = ? AND password = ? LIMIT 1");
-                $verify_user->execute ([$email, $c_password]);
-                $row = $verify_user->fetch(PDO::FETCH_ASSOC);
+            // if($insert_user){
+            //     $verify_user = $conn->prepare("SELECT * FROM property_owner WHERE email = ? AND password = ? LIMIT 1");
+            //     $verify_user->execute ([$email, $c_password]);
+            //     //$row = $verify_user->fetch(PDO::FETCH_ASSOC);
 
-                if($verify_user->rowCount() > 0){
-                    //setcookie('user_id', $row['user_id'], time() + 60*60*24*30, '/');
-                    
-                    $success_msg[] = 'Account Created Successfully';
-                    header('location:ologin.php');
-                }else{
-                    //$error_msg[] = 'something went wrong!';
-                }
+            //     if($verify_user->rowCount() > 0){
+            //         setcookie('user_id', $row['user_id'], time() + 60*60*24*30, '/');
+            //         $success_msg[] = 'Account Created Successfully';
+            //         header('location:ologin.html');
+            //         exit();
+            //     }else{
+            //         $error_msg[] = 'something went wrong!';
+            //     }
 
-            }
+            // }
         }
 
     }
@@ -63,115 +59,3 @@ if(isset($_POST['submit'])){
 }
 
 ?>
-
-
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>signup</title>
-    <link rel="stylesheet" href="../style.css"/>
-</head>
-<body>
-    <!--Navigation Bar here-->
-    <div class="nav-container">
-        <div class="wrapper">
-            <nav>
-                <div class="logo">
-                    Africa
-                </div>
-                <ul class="nav-items">
-                    <li>
-                        <a href="home.php">Home</a>
-                    </li>
-
-                    <li>
-                        <a href="ologin.php">Signin</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-
-
-    <main>
-        <!--  home section starts -->
-    <section class="home">
-        <div class="home-slide">
-            <div class="wrapper">
-                <div class="slide" style="background:url(../asset/image/home1.jpg) no-repeat">
-                    <div class="content">
-                        <h3>Welcome To Africa Help</h3>
-                        <span>home away from home</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--  home section end -->
-
-    <div class="title">
-        <h1 class="heading-title">Sign Up</h1>
-    </div>
-    <section class="form-container">
-        <form action="" method="POST">
-            <h3>create an account now</h3>
-            <input type="text" name="firstname" required placeholder="First Name" 
-            class="box">
-            <input type="text" name="lastname" required placeholder="Last Name" 
-            class="box">
-            <input type="email" name="email" required placeholder="Email" 
-            class="box">
-            <input type="phone" name="phone" required placeholder="Phone" 
-            class="box">
-            <input type="password" name="password" required placeholder="Password" 
-            class="box">
-            <input type="password" name="c_password" required placeholder="Confirm Password" 
-            class="box">
-            <p>already have an account? <a href="ologin.php">login now</a></p>
-            <input type="submit" value="SignUp" name="submit" class="btn">
-        </form>
-    </section>
-
-    </main>
-
-    <!--Footer Start-->
-    <Footer>
-        <div class="footer-content">
-            <h3>Contact us</h3>
-            <p>Telephone: +44-700-000-000</p>
-            <p>Mobile: +44-700-000-000</p>
-            <p>Email: dyu@gmail.com</p>
-            <ul class="socials">
-                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-            </ul>
-        </div>
-        <div class="footer-bottom">
-            <p>copyright &copy;2022 Olabisi. Created by <span>2201917</span> </p>
-
-        </div>
-    </Footer>
-    <!--Footer Ends-->
-
-
-
-
-<!-- sweetalert cdn link-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
-
-<!-- personal script-->
-<script src="script.js"></script>
-
-<?php include 'component/message.php' ?>
-
-
-</body>
-</html>
